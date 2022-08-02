a3570c94c183accd4a4c5fc0ce052a23bc0a3f3e
ys4510 <ystaich@gmail.com>
2022-08-01 18:33:35 -0700

    TodoPage added

--- src/App.tsx
+++ src/App.tsx
@@ -1,83 +1,13 @@
-import React, { useState, FC } from "react";
-import {Todo} from './Tyeps'
-import { v4 as uuidv4 } from 'uuid';
-// import FormInput from './components/FormInput';
-// import FormList from './components/FormList';
+import { FC } from "react";
 import "./App.css";
+import TodoPage from "./TodoPage";
 
 const App: FC = () => {
-
-  // const InitialTodos: Todo[] = [
-  //   {
-  //     id: uuidv4(),
-  //     title: 'Title A',
-  //     isCompleted: false
-  //   },
-  //   {
-  //     id: uuidv4(),
-  //     title: 'Title B',
-  //     isCompleted: false
-  //   }
-  // ];
-  const InitialTodos: Todo[] = [];
-
-  const [todos, setTodos] = useState(InitialTodos);
-  const [titleInput, setTitleInput] = useState('');
-
-  const handleOnChangeStatus = (todo: Todo )  => {
-    setTodos((prev) =>  prev.map( t => todo.id === t.id  ? {...todo, isCompleted: !todo.isCompleted} :t));
-  }
-
-  const handleInputChange = (e: React.ChangeEvent<HTMLInputElement>) => { 
-    setTitleInput(e.target.value)
-  }
-
-  const handleOnSubmit = () => {
-    if (titleInput) {
-      const newTodo : Todo = {
-        id: uuidv4(),
-        title: titleInput,
-        isCompleted: false
-      }
-      setTodos(todo => [...todo, newTodo])
-      setTitleInput('')
-    }
-  }
-
   return (
     <div className="App">
-      <h3>Vite + React + TS</h3>
-      <h1>簡易Todoリスト</h1>
-      <div id="inputForm">
-        <label>
-          {"タイトル : "}
-          <input type="text" value={titleInput} onChange={handleInputChange}  />
-        </label>
-        <input className='btn' type="submit" value="作成" onClick={handleOnSubmit}/>
-      </div>
-      <hr style={{ border: "1px dashed yellowgreen" }} />
-      <table>
-        <thead>
-          <tr>
-            <td>ID</td>
-            <td>タイトル</td>
-            <td>進捗</td>
-          </tr>
-        </thead>
-        <tbody>
-          { todos.length <= 0 ? "データなし" : todos.map( (t, i) => {
-            return (
-              <tr key={i}>
-              <td className= {t.isCompleted ? "checked" : ''}>{t.id}</td>
-              <td className= {t.isCompleted ? "checked" : ''}>{t.title}</td>
-              <td><input type="checkbox" checked={t.isCompleted} onChange={()=> handleOnChangeStatus(t) }/></td>
-            </tr>
-              )
-          })}
-        </tbody>
-      </table>
+      <TodoPage />
     </div>
   );
-}
+};
 
 export default App;
