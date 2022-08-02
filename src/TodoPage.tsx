b15844d67996784c91f7a0754867cc5332d8bf29
ys4510 <ystaich@gmail.com>
2022-08-01 19:34:25 -0700

    Header added

--- src/TodoPage.tsx
+++ src/TodoPage.tsx
@@ -1,46 +1,54 @@
 import React, { useState, FC } from "react";
-import {Todo} from './Tyeps'
-import { v4 as uuidv4 } from 'uuid';
+import { Todo } from "./Tyeps";
+import Header from "./components/Header";
+import { v4 as uuidv4 } from "uuid";
 // import FormInput from './components/FormInput';
 // import FormList from './components/FormList';
 
 const TodoPage: FC = () => {
-
   const InitialTodos: Todo[] = [];
 
   const [todos, setTodos] = useState(InitialTodos);
-  const [titleInput, setTitleInput] = useState('');
+  const [titleInput, setTitleInput] = useState("");
 
-  const handleOnChangeStatus = (todo: Todo )  => {
-    setTodos((prev) =>  prev.map( t => todo.id === t.id  ? {...todo, isCompleted: !todo.isCompleted} :t));
-  }
+  const handleOnChangeStatus = (todo: Todo) => {
+    setTodos((prev) =>
+      prev.map((t) =>
+        todo.id === t.id ? { ...todo, isCompleted: !todo.isCompleted } : t
+      )
+    );
+  };
 
-  const handleInputChange = (e: React.ChangeEvent<HTMLInputElement>) => { 
-    setTitleInput(e.target.value)
-  }
+  const handleInputChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+    setTitleInput(e.target.value);
+  };
 
   const handleOnSubmit = () => {
     if (titleInput) {
-      const newTodo : Todo = {
+      const newTodo: Todo = {
         id: uuidv4(),
         title: titleInput,
-        isCompleted: false
-      }
-      setTodos(todo => [...todo, newTodo])
-      setTitleInput('')
+        isCompleted: false,
+      };
+      setTodos((todo) => [...todo, newTodo]);
+      setTitleInput("");
     }
-  }
+  };
 
   return (
     <div className="App">
-      <h3>Vite + React + TS</h3>
-      <h2>簡易Todoリスト</h2>
+      <Header />
       <div id="inputForm">
         <label>
           {"タイトル : "}
-          <input type="text" value={titleInput} onChange={handleInputChange}  />
+          <input type="text" value={titleInput} onChange={handleInputChange} />
         </label>
-        <input className='btn' type="submit" value="作成" onClick={handleOnSubmit}/>
+        <input
+          className="btn"
+          type="submit"
+          value="作成"
+          onClick={handleOnSubmit}
+        />
       </div>
       <hr style={{ border: "1px dashed yellowgreen" }} />
       <table>
@@ -52,19 +60,29 @@ const TodoPage: FC = () => {
           </tr>
         </thead>
         <tbody>
-          { todos.length <= 0 ? "データなし" : todos.map( (t, i) => {
-            return (
-              <tr key={i}>
-              <td className= {t.isCompleted ? "checked" : ''}>{t.id}</td>
-              <td className= {t.isCompleted ? "checked" : ''}>{t.title}</td>
-              <td><input type="checkbox" checked={t.isCompleted} onChange={()=> handleOnChangeStatus(t) }/></td>
-            </tr>
-              )
-          })}
+          {todos.length <= 0
+            ? "データなし"
+            : todos.map((t, i) => {
+                return (
+                  <tr key={i}>
+                    <td className={t.isCompleted ? "checked" : ""}>{t.id}</td>
+                    <td className={t.isCompleted ? "checked" : ""}>
+                      {t.title}
+                    </td>
+                    <td>
+                      <input
+                        type="checkbox"
+                        checked={t.isCompleted}
+                        onChange={() => handleOnChangeStatus(t)}
+                      />
+                    </td>
+                  </tr>
+                );
+              })}
         </tbody>
       </table>
     </div>
   );
-}
+};
 
 export default TodoPage;
---
+++ src/components/Header.tsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+const Header = () => {
+  return (
+    <div>
+      <h3>Vite + React + TS</h3>
+      <h2>簡易Todoリスト!</h2>
+    </div>
+  );
+};
+
+export default Header;
